6a4edb74371aaa696793f2a9d31ade21a3c023c4
SudewaJ <it20062774@my.sliit.lk>
2026-02-08 13:53:25 +0530

    fix: switch auth to jwt and update types for admin login

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,17 +1,26 @@
-// types/next-auth.d.ts
-import NextAuth from "next-auth";
+import "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+      isPro: boolean;
+      createdAt?: Date;
+    } & DefaultSession["user"];
   }
 
   interface User {
     id: string;
+    isPro?: boolean;
+    createdAt?: Date;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    isPro?: boolean;
+    createdAt?: Date;
   }
 }
